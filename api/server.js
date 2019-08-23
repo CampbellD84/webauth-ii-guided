3546aeee691a834229c05084af815d5c51a7e960
Douglas Campbell <dacampbelltbone@gmail.com>
2019-08-22 23:28:36 -0400

    Completed guided project

--- api/server.js
+++ api/server.js
@@ -1,9 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+
+const sessionOptions = {
+  name: "mycookie",
+  secret: "cookiesareyumyummewantcookies",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require("../database/dbConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
 
 const server = express();
 
@@ -11,11 +33,13 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use(session(sessionOptions));
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
